7dab62fbe7c8aae04cea2ce11b4476e88af1c720
foontype <hogetype@gmail.com>
2025-01-22 22:19:31 +0000

    update

---
+++ run/project-creator/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+cd $(dirname "${0}")
+
+source ../supports/bask/src/bask.sh
+
+bask_default_task="usage"
+
+task_usage() {
+        bask_list_tasks
+}
+
+task_create() {
+        local new_project_name="${NEW_PROJECT_NAME:?}"
+        local new_project_export_dir="${NEW_PROJECT_EXPORT_DIR:-../../..}"
+        local new_project_dir="${new_project_export_dir}/${new_project_name}"
+        local export_path="../.."
+
+        mkdir -p "${project_dir}"
+        (cd "${export_path}" && git archive --format=tar HEAD) \
+                | tar -xvf - -C "${new_project_dir}"
+}
