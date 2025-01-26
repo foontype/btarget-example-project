14849754eee58ab9f0a0862ca2d30180c6353262
foontype <hogetype@gmail.com>
2025-01-26 12:21:09 +0000

    update

--- run/shell-tester/run.sh
+++ run/shell-tester/run.sh
@@ -2,11 +2,6 @@
 set -e
 cd $(dirname "${0}")
 
-EXCLUDE_PATHS=(
-    "./run/supports/bask/*"
-    "./run/supports/btarget/*"
-)
-
 source ../supports/bask/src/bask.sh
 
 bask_default_task="usage"
@@ -16,30 +11,17 @@ task_usage() {
 }
 
 task_test() {
-    cd ../..
-    _bats_docker
-}
-
-_bats_docker() {
-    docker run --rm -it \
-        -v $(_os_path $(pwd)):/workspace \
-        -w "/workspace" \
-        -e "WORKSPACE_ROOT=/workspace" \
+    cd ../../
+    docker run \
+        --rm \
+        -it \
+        -v $(_os_path $(pwd)):/code \
+        -w "/code" \
         bats/bats:1.11.1 \
         --print-output-on-failure \
         --pretty \
         --recursive \
-        $(_list_bats_files)
-}
-
-_list_bats_files() {
-    eval find . -type f -name "*.sh.bats" $(_exclude_paths)
-}
-
-_exclude_paths() {
-    for p in "${EXCLUDE_PATHS[@]}"; do
-        echo "-a -not -path \"${p}\""
-    done
+        ./run/shell-tester/tests
 }
 
 _os_path() {
