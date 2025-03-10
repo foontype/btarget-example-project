5e1128854ff4890ed1ef932fad40193826f3d5a8
foontype <hogetype@gmail.com>
2025-03-10 14:42:00 +0000

    update

--- run/devcontainer/containers/workspace/init-container/103-init-claude-code-mcp.sh
+++ run/devcontainer/containers/workspace/init-container/103-init-claude-code-mcp.sh
@@ -2,6 +2,21 @@
 
 if [ -n "${INIT_CONTAINER_CLAUDE_CODE}" -a -n "${INIT_CONTAINER_UV}" ]; then
     # https://www.claudemcp.com/servers
-    # example) claude mcp add mcp-server-fetch uvx mcp-server-fetch
-    claude mcp add mcp-server-fetch uvx mcp-server-fetch
+
+    # mcp-server-fetch
+    claude mcp add mcp-server-fetch \
+        -- uvx mcp-server-fetch
+
+    # mcp-shell-server
+    claude mcp add mcp-shell-sever \
+        -e ALLOW_COMMANDS="${MCP_SHELL_SERVER_ALLOW_COMMANDS:-ls,cat,pwd,grep,wc,touch,find}" \
+        -- uvx mcp-shell-server
+
+    # git
+    claude mcp add mcp-server-git \
+        -- uvx mcp-server-git --repository /workspace
+
+    # memory
+    claude mcp add memory \
+        -- npx -y @modelcontextprotocol/server-memory
 fi
