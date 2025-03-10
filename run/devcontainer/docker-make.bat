3430c01cc8572c73c5340079c78567d50278697a
foontype <hogetype@gmail.com>
2025-03-10 08:53:26 +0000

    update

--- run/devcontainer/docker-make.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-type Dockerfile.base Dockerfile.workspace Dockerfile.user-workspace > Dockerfile
--- run/devcontainer/docker-make.sh
+++ run/devcontainer/docker-make.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
 cd $(dirname "${0}")
 cat ./Dockerfile.base ./Dockerfile.workspace ./Dockerfile.user-workspace > ./Dockerfile
