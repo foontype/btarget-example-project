1d23341718e4d4c02f29dd5d71ce27a8dc0317f1
foontype <hogetype@gmail.com>
2025-03-21 12:26:29 +0000

    update

--- run/devcontainer/containers/workspace/init-container/900-init-workspace.sh
+++ run/devcontainer/containers/workspace/init-container/900-init-workspace.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ -n "${INIT_CONTAINER_WORKSPACE}" ]; then
-    bash /workspace/run/target.sh init
+    RUN_TARGET_SELECT=devcontainer bash /workspace/run/target.sh init
 fi
